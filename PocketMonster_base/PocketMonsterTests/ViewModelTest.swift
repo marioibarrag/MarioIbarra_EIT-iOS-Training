bac49966cc65de439f0012e7e1c8c7f0f9bafdee
marioibarrag <mario.ibarrag@hotmail.com>
2022-04-27 10:30:04 -0400

    added png file

--- PocketMonster_base/PocketMonsterTests/ViewModelTest.swift
+++ PocketMonster_base/PocketMonsterTests/ViewModelTest.swift
@@ -20,7 +20,7 @@ class ViewModelTest: XCTestCase {
             try await viewModel.loadPokemons()
             let pokemonAbilities = [PokemonAbility(ability: PokemonAbility.Ability(name: "overgrow")), PokemonAbility(ability: PokemonAbility.Ability(name: "chlorophyll"))]
             let pokemon = await PokemonData(id: 1, name: "bulbasaur", imgURL: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png", abilities: pokemonAbilities, imgData: try viewModel.networkManager.getData())
-            
+            print("POKEMON: \(viewModel.pokemons[0])")
             XCTAssertEqual(pokemon, viewModel.pokemons[0])
             
         } catch {
